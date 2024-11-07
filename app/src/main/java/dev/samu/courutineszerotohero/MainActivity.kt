bef9655c963c91c62271e4ac845178e8cdc5b6d6
SamuelRiveira <samuelriveiraescudero@gmail.com>
2024-11-07 16:44:27 +0000

    Limpiado import inutiles

--- app/src/main/java/dev/samu/courutineszerotohero/MainActivity.kt
+++ app/src/main/java/dev/samu/courutineszerotohero/MainActivity.kt
@@ -8,7 +8,6 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -18,11 +17,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import dev.samu.courutineszerotohero.ui.theme.CourutineszerotoheroTheme
-import androidx.lifecycle.lifecycleScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
-import retrofit2.Response
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
