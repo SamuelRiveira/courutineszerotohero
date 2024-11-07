852423d7e511c905e050944958f821e2a5fc36f7
SamuelRiveira <samuelriveiraescudero@gmail.com>
2024-11-07 16:45:07 +0000

    Solucionado error importando manualmente import androidx.compose.foundation.lazy.items

--- app/src/main/java/dev/samu/courutineszerotohero/MainActivity.kt
+++ app/src/main/java/dev/samu/courutineszerotohero/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
